4f1bc7440c87888fe06c7d17fe35b0614c5b6aab
UzomaAkpala <francisakpala@yahoo.com>
2023-12-20 14:48:44 +0100

    fetching data using Axios

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const fetchData = async () => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "65095b61",
+      s: "avengers",
+    },
+  });
+  console.log(response.data);
+};
 
+fetchData();
