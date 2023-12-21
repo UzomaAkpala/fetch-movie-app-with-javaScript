40922a77c5ea4fe7b075771af0ea54d0ec8927a9
UzomaAkpala <francisakpala@yahoo.com>
2023-12-21 13:57:04 +0100

    Making a follow up request

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ const onInput = async (event) => {
     option.addEventListener("click", () => {
       dropdown.classList.remove("is-active");
       input.value = movie.Title;
+      onMovieSelect(movie);
     });
 
     resultWrapper.appendChild(option);
@@ -60,3 +61,13 @@ document.addEventListener("click", (event) => {
     dropdown.classList.remove("is-active");
   }
 });
+
+const onMovieSelect = async (movie) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "65095b61",
+      i: movie.imdbID,
+    },
+  });
+  console.log(response.data);
+};
